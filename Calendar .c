bf5360c9a3346cd01047a11b8f925e265680f9ef
HASAN RAFI AHMED <108696119+Rafi570@users.noreply.github.com>
2024-12-22 23:22:00 +0600

    Update Calendar .c

--- Calendar .c
+++ Calendar .c
@@ -1,51 +1,47 @@
-#include<stdio.h>
-int getFirstDayofTheYear(int year){
-    int day=(year*365+((year-1)/4((year-1)/100+((year-1)/400))%7;
+#include <stdio.h>
+
+int getFirstDayofTheYear(int year) {
+    int day = (year * 365 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400) % 7;
     return day;
 }
 
-int main(){
-    char months[]={'January','February','March','Appril','May','June','July','August','September','October','November','December'};
+int main() {
+    char *months[] = {"January", "February", "March", "April", "May", "June", "July", 
+                      "August", "September", "October", "November", "December"};
+    int daysinMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int i, j, totalDays, weekday = 0, spaceCounter = 0, year;
 
-    int daysinMonth[]={31,28,31,30,31,30,31,31,30,31,30,31};
-    int i,j,totalDays , weekday=0,spaceCounter=0,year;
-    printf('Enter Your favourite year :\n')
-    scanf('%d'&year);
+    printf("Enter your favorite year:\n");
+    scanf("%d", &year);
 
-    printf('Wecome to ',year)
+    printf("Welcome to %d\n", year);
 
-    // checking for leap year 
-
-    if((year%4=00 && year%100==0)||(year%400==0)){
-        daysinMonth[1]=29;
-
+    // Check for leap year
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        daysinMonth[1] = 29;
     }
 
-    // get the first day of the year 
-    weekday=getFirstDayofTheYear(year);
+    // Get the first day of the year
+    weekday = getFirstDayofTheYear(year);
 
+    for (i = 0; i < 12; i++) {
+        printf("\n\n\n-------%s------\n", months[i]);
+        printf("\n Sun Mon Tue Wed Thu Fri Sat \n");
 
-    for(i=0;i<12;i++){
-        printf('\n\n\n-------%s------\n',months[i]);
-        printf('\n sun mon thu wed thu fri sat \n');
-        for(spaceCounter=1;spaceCounter<=weekday;spaceCounter++){
-            printf(' ');
+        for (spaceCounter = 1; spaceCounter <= weekday; spaceCounter++) {
+            printf("    "); // 4 spaces
         }
 
-        totalDays=daysinMonth[i];
-        for(j=1;j<=totalDays;j++){
-            printf('%d',j);
+        totalDays = daysinMonth[i];
+        for (j = 1; j <= totalDays; j++) {
+            printf("%4d", j); // 4 spaces
             weekday++;
-            if (weekday>6)
-            {
-                weekday=0
-                printf('\n')
+            if (weekday > 6) {
+                weekday = 0;
+                printf("\n");
             }
-            
         }
-
     }
 
     return 0;
-
 }
